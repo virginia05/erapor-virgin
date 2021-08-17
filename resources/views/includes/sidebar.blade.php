aab3f51bcd65838cb705280ee0984be0c8ac9dc8
mail21 <ismailidn1@gmail.com>
2021-08-17 20:49:53 +0700

    aa

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -3,15 +3,15 @@
         <li class="my-2"><a href="{{url('/menu-guru')}}">Kelola Nilai Siswa</a></li>
         <li class="my-2"><a href="{{url('/absensi')}}">Kelola Absensi</a></li>
         <li class="my-2"><a href="#">Lihat Nilai</a></li>
-        <li class="my-2">Kelola Master</li>
+        <li class="my-2"><a href="{{url('/ledger?tahun_ajaran=2021/2022&kode_rombel=1')}}">Lihat Ledger</a></li>
+        <li class="my-2"><a href="#">Profile</a></li>
+        <li class="my-2">Kelola Data</li>
         <li class="my-2"><a href="{{url('/kelas')}}">Kelas</a></li>
         <li class="my-2"><a href="{{url('/mapel')}}">Mapel</a></li>
         <li class="my-2"><a href="{{url('/beban')}}">Beban Ajar</a></li>
-        <li class="my-2">Kelola User</li>
         <li class="my-2"><a href="{{url('/siswa')}}">Siswa</a></li>
         <li class="my-2"><a href="{{url('/guru')}}">Guru</a></li>
-        <li class="my-2"><a href="{{url('/ledger?tahun_ajaran=2021/2022&kode_rombel=1')}}">Lihat Ledger</a></li>
-        <li class="my-2"><a href="#">Profile</a></li>
+        
         <li class="my-2"><a href="/login">Keluar</a></li>
     </ul>
 {{-- </nav> --}}
